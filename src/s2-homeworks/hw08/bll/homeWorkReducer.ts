80f280edb6c1d58044a2e7378b6f1bb61546361c
KingShakeJS <kingshakejs@yandex.ru>
2024-12-21 19:08:41 +0300

    gotovo-hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,27 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            const stateCopy = [...state]
+
+            if (action.payload === 'up') {
+                return stateCopy.sort((a, b) => a.name.localeCompare(b.name));
+
+            } else {
+                return stateCopy.sort((a, b) => b.name.localeCompare(a.name));
+
+            }
+
         }
         case 'check': {
+            console.log(action.payload)
 
-            return state // need to fix
+
+
+            return state.filter(i => i.age >= action.payload) // need to fix
         }
         default:
             return state
