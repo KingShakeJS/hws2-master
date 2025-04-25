a1730b1bd78eddd4b1bfe63b6a081593e94f9b55
KingShakeJS <kingshakejs@yandex.ru>
2025-04-25 15:28:43 +0300

    gotovo-hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,13 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type stateType = typeof initState
+
+export const loadingReducer = (state:stateType = initState, action: LoadingActionType): stateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+            case 'CHANGE_LOADING':
+                return {...state, isLoading: action.isLoading}
         default:
             return state
     }
