486f897417e51183164af10efeaf88a1681c0505
KingShakeJS <kingshakejs@yandex.ru>
2025-04-25 17:29:12 +0300

    gotovo-hw12

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,38 +1,23 @@
-import React, {ChangeEvent, useState} from 'react'
-import s from './HW11.module.css'
-import s2 from '../../s1-main/App.module.css'
-import {restoreState} from '../hw06/localStorage/localStorage'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-
-
-/*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+import React, { useState } from 'react';
+import s from './HW11.module.css';
+import s2 from '../../s1-main/App.module.css';
+import { restoreState } from '../hw06/localStorage/localStorage';
+import SuperRange from './common/c7-SuperRange/SuperRange';
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0));
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100));
 
     const change = (event: Event, value: number | number[]) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        if (event.target) {
-            if (typeof value === 'object') {
-
-
-                setValue1(+event.target)
-                setValue2(+event.target)
-            } else {
-
-
-                setValue1(+event.target)
-            }
+        if (Array.isArray(value)) {
+            // Если value - массив, обновляем оба слайдера
+            setValue1(value[0]);
+            setValue2(value[1]);
+        } else {
+            // Если value - одно число, обновляем только первый слайдер
+            setValue1(value);
         }
-
-
-    }
+    };
 
     return (
         <div id={'hw11'}>
@@ -44,27 +29,23 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
                             value={value1}
-                            onChange={(event) => change(event, value1)}
-
+                            onChange={(event, value) => change(event, value)} // Передаем значение напрямую
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-                            value={value2}
-                            onChange={(event) => change(event, [value1, value2])}
-
+                            value={[value1, value2]} // Передаем оба значения для двойного слайдера
+                            onChange={(event, value) => change(event, value)} // Передаем значение напрямую
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default HW11
+export default HW11;
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,25 @@
 const initState = {
     themeId: 1,
 }
+type stateType = typeof initState
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type changeThemeIdAT = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const themeReducer = (state: stateType = initState, action: changeThemeIdAT): stateType => { // fix any
     switch (action.type) {
         // дописать
+        case "SET_THEME_ID":
+            return {
+                ...state,
+                themeId: action.id
+            }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdAT => ({type: 'SET_THEME_ID', id}) // fix any
